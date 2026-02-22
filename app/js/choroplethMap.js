000c4b22c768f71071dc637eca7a8527d75b3780
Matthew Goldsberry <mattheweg99@gmail.com>
2026-02-21 20:59:30 -0500

    add choropleth map

---
+++ app/js/choroplethMap.js
@@ -0,0 +1,221 @@
+/**
+ * ChoroplethMap object class 
+ */
+class ChoroplethMap {
+
+    /**
+     * Class constructor with basic scatterplot configuration
+     * @param {Object} _config
+     *  - parentElement: DOM element for SVG container
+     *  - containerWidth: width of SVG container
+     *  - containerHeight: height of SVG container
+     *  - margin: definition of top, right, left and bottom margins
+     *  - legendBottom: pixels from bottom of SVG to legend position
+     *  - legendLeft: pixes from the left of SVG to legend position
+     *  - legendRectHeight: height of legend bar
+     *  - legendRectWidth: length of legend bar
+     *  - legendLabel: label for legend
+     * @param {Array} _data
+     */
+    constructor(_config, _data) {
+        this.config = {
+            parentElement: _config.parentElement,
+            containerWidth: _config.containerWidth || 500,
+            containerHeight: _config.containerHeight || 300,
+            margin: _config.margin || { top: 0, right: 0, bottom: 0, left: 0 },
+            legendBottom: _config.legendBottom || 25,
+            legendLeft: _config.legendLeft || 0,
+            legendRectHeight: _config.legendRectHeight || 10,
+            legendRectWidth: _config.legendRectWidth || 250,
+            legendLabel: _config.legendLabel,
+        }
+        this.data = _data;
+        this.initVis();
+    }
+
+    /**
+     * Initialize the histogram
+     */
+    initVis() {
+        let vis = this;
+
+        // calculate inner chart size
+        vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
+        vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
+        vis.mapHeight = vis.height - 40
+
+        // define size of SVG drawing area based on the specified SVG window 
+        vis.svg = d3.select(vis.config.parentElement)
+            .attr('viewBox', `0 0 ${vis.config.containerWidth} ${vis.config.containerHeight}`)
+            .attr('preserveAspectRatio', 'xMidYMid meet');
+
+        // append group element that will contain our actual chart and position it according to the given margin config
+        vis.chart = vis.svg.append('g')
+            .attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
+
+        // initialize projection and path generator
+        vis.projection = d3.geoMercator();
+        vis.geoPath = d3.geoPath().projection(vis.projection);
+
+        // initialize the colors used in chart
+        vis.colors = ['#ffffd9', '#edf8b1', '#c7e9b4', '#7fcdbb', '#41b6c4', '#1d91c0', '#225ea8', '#253494', '#172976', '#081d58']
+        vis.colorScale = d3.scaleQuantize()
+            .range(vis.colors);
+
+        // append pattern with diagonal lines to signify that no data was provided for that country
+        vis.svg.append('defs')
+            .append('pattern')
+            .attr('id', 'lightstripe')
+            .attr('patternUnits', 'userSpaceOnUse')
+            .attr('width', 4)
+            .attr('height', 4)
+            .append('path')
+            .attr('d', 'M 0,0 L 4,4') 
+            .attr('stroke', 'grey') 
+            .attr('stroke-width', 0.5); 
+
+
+        // initialize gradient that will later be use for the legend
+        vis.linearGradient = vis.svg.append('defs').append('linearGradient')
+            .attr("id", "legend-gradient");
+
+        // append legend color guide
+        const totalLegendWidth = 35 + vis.config.legendRectWidth; // shift 35 pixels right to account for no data legend value
+        vis.legend = vis.chart.append('g')
+            .attr('class', 'legend')
+            .attr('transform', `translate(${vis.width / 2 - totalLegendWidth / 2},${vis.height - vis.config.legendBottom})`);
+
+        // label legend
+        vis.legendTitle = vis.legend.append('text')
+            .attr('class', 'legend-title')
+            .attr('font-size', '.55rem')
+            .attr('text-anchor', 'middle') // Add this
+            .attr('x', ((vis.config.legendRectWidth + 35) / 2)) // Center it here
+            .attr('y', -5)
+            .text(vis.config.legendLabel);
+
+        // create sub-legend for no data key
+        vis.noDataLegend = vis.legend.append('g'); 
+
+        // fill legend value with lightstrip (diagonal lines)
+        vis.noDataLegend.append('rect')
+            .attr('width', 20)
+            .attr('height', vis.config.legendRectHeight)
+            .attr('fill', 'url(#lightstripe)')
+            .attr('stroke', 'black')
+            .attr('stroke-width', 0.5);
+
+        // add "No Data" as label to sub-legend
+        vis.noDataLegend.append('text')
+        .attr('class', 'no-data-label')
+            .attr('x', 10)
+            .attr('y', 20)
+            .attr('text-anchor', 'middle')
+            .attr('font-size', '.5rem')
+            .attr('dy', '.35em')
+            .text('No Data');
+
+        // setup legend bin blocks to the right of the no data legend
+        vis.legendBlocks = vis.legend.append('g')
+            .attr('transform', 'translate(35, 0)');
+
+        // render initial visualization
+        vis.updateVis();
+    }
+
+    /**
+     * Update the visualization 
+     */
+    updateVis() {
+        let vis = this;
+
+        // calculate the extent of the values
+        vis.valueDensityExtent = d3.extent(vis.data.features, d => d.properties.value);
+
+        // update color scale based on data
+        vis.colorScale.domain(vis.valueDensityExtent);
+
+        // render histogram
+        vis.renderVis();
+    }
+
+    /**
+     * Render the visualizations
+     */
+    renderVis() {
+        let vis = this;
+
+        // defines the scale of the projection so that the geometry fits within the SVG area
+        vis.projection.fitSize([vis.width, vis.mapHeight], vis.data);
+
+        // append world map
+        const countryPath = vis.chart.selectAll('.country')
+            .data(vis.data.features)
+            .join('path')
+            .attr('class', 'country')
+            .attr('d', vis.geoPath)
+            .attr('fill', d => {
+                if (d.properties.value) {
+                    return vis.colorScale(d.properties.value);
+                } else {
+                    return 'url(#lightstripe)';
+                }
+            })
+            .attr('stroke', 'black')
+            .attr('stroke-width', 0.5);
+
+        // calculate tick values
+        const domain = vis.colorScale.domain();
+        const thresholds = vis.colorScale.thresholds();
+        const tickVals = [domain[0], ...thresholds, domain[1]];
+
+        // calculate width of each block in legend bar
+        const blockWidth = vis.config.legendRectWidth / vis.colors.length;
+
+        // add each block that represents a bin to the overall legend bar
+        vis.legendBlocks.selectAll('.legend-block')
+            .data(vis.colors)
+            .join('rect')
+            .attr('class', 'legend-block')
+            .attr('x', (d, i) => i * blockWidth)
+            .attr('y', 0)
+            .attr('width', blockWidth)
+            .attr('height', vis.config.legendRectHeight)
+            .attr('fill', d => d);
+
+        // add a border around all of the legend blocks
+        vis.legendBlocks.selectAll('.legend-border')
+            .data([1])
+            .join('rect')
+            .attr('class', 'legend-border')
+            .attr('width', vis.config.legendRectWidth)
+            .attr('height', vis.config.legendRectHeight)
+            .attr('fill', 'none')
+            .attr('stroke', 'black')
+            .attr('stroke-width', 0.5);
+
+        // add tick lines to more clearly separate each bin
+        vis.legendBlocks.selectAll('.legend-tick')
+            .data(tickVals)
+            .join('line')
+            .attr('class', 'legend-tick')
+            .attr('x1', (d, i) => i * blockWidth)
+            .attr('x2', (d, i) => i * blockWidth)
+            .attr('y1', 0)
+            .attr('y2', vis.config.legendRectHeight + 2)
+            .attr('stroke-width', 0.25);
+
+        // add legend labels at each bin 
+        vis.legend.selectAll('.legend-label')
+            .data(tickVals)
+            .join('text')
+            .attr('class', 'legend-label')
+            .attr('text-anchor', 'middle')
+            .attr('font-size', '.5rem')
+            .attr('dy', '.35em')
+            .attr('y', 20)
+            .attr('y', vis.config.legendRectHeight + 10)
+            .attr('x', (d, i) => 35 + (i * blockWidth))
+            .text(d => d3.format(".0f")(d));
+    }
+}
--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,7 @@
  * Create and render Histogram for a specific variable and year
  * @param {Array<Object>} data - dataset loaded from CSV
  * @param {string} valueKey - name of column to be used for binning values
- * @param {string} year - year used to filter data
+ * @param {number} year - year used to filter data
  * @param {Histogram} histogram - reference to histogram instance
  * @param {string} parentElement - css selector for target SVG element
  * @param {string} chartTitle - title of the chart
@@ -29,7 +29,7 @@ function updateHistogram(data, valueKey, year, histogram, parentElement, chartTi
  * @param {Array<Object>} data - dataset loaded from CSV
  * @param {string} xValueKey - name of column to be used for x axis
  * @param {string} yValueKey - name of column to be used for y axis
- * @param {string} year - year used to filter data
+ * @param {number} year - year used to filter data
  * @param {Scatterplot} scatterplot - reference to Scatterplot instance
  * @param {string} parentElement - css selector for target SVG element
  */
@@ -47,31 +47,104 @@ function updateScatterplot(data, xValueKey, yValueKey, year, scatterplot, parent
     scatterplot.updateVis();
 }
 
+
+// mapping for inconsistent country names between datasets {nameFromCSV : nameFromGeoJSON}
+const countryNameMapping = {
+    'United States': 'USA',
+    'United States': 'USA',
+    "Cote d'Ivoire": 'Ivory Coast',
+    'Czechia': 'Czech Republic',
+    'Democratic Republic of Congo': 'Democratic Republic of the Congo',
+    'Tanzania': 'United Republic of Tanzania',
+    'Serbia': 'Republic of Serbia',
+    'Bahamas': 'The Bahamas',
+    'Congo': 'Republic of the Congo',
+    'Eswatini': 'Swaziland',
+    'North Macedonia': 'Macedonia',
+    'Guinea-Bissau': 'Guinea Bissau',
+    'Palestine': 'West Bank',
+    'United Kingdom': 'England'
+}
+
+/**
+ * Maps country name from geojson to name as it appears in the CSV.
+ * @param {string} name - name to map to CSV name
+ * @returns - name that exists in the CSV
+ */
+function normalizeCountryName(name) {
+    return countryNameMapping[name] || name;
+}
+
+/**
+ * Create and render ChoroplethMap for a specific variable pair and year
+ * @param {Array<Object>} data - dataset loaded from CSV
+ * @param {Array<Object>} geoData - topo dataset loaded from JSON
+ * @param {string} valueKey - name of column to be used for binning values
+ * @param {number} year - year used to filter data
+ * @param {ChoroplethMap} choroplethMap - reference to ChoroplethMap instance
+ * @param {string} parentElement - css selector for target SVG element
+ * @param {string} legendLabel - name of legend
+ */
+function updateChoroplethMap(data, geoData, valueKey, year, choroplethMap, parentElement, legendLabel) {
+    // filter data based on provided year and valueKey
+    const filteredData = data.filter(d => d.year === year);
+    filteredData.forEach(d => d.value = +d[valueKey]);
+
+    // removed Antarctica (will never contain info and its removal allows for zoomed in views of other countries)
+    geoData.features = geoData.features.filter(d => d.properties.name !== 'Antarctica')
+
+    // combine both datasets by adding the value to file
+    geoData.features.forEach(d => {
+        const matchingCountry = filteredData.find(country => normalizeCountryName(country.entity) === d.properties.name);
+
+        if (matchingCountry) {
+            d.properties.value = +matchingCountry[valueKey];
+        } else {
+            d.properties.value = null;
+        }
+    });
+
+    // initialize and render choropleth map
+    const config = { parentElement, legendLabel: legendLabel };
+    choroplethMap = new ChoroplethMap(config, geoData);
+    choroplethMap.updateVis();
+}
+
 /**
  * Load data from CSV files and initialize charts
  */
 
 // chart references
-let sanitation_histogram;
-let child_mortality_histogram;
+let sanitationHistogram;
+let childMortalityHistogram;
 let scatterplot;
+let choroplethMap;
 
 // load dataset
-d3.csv('data/child_mortality_trends.csv')
+Promise.all([
+    d3.json('data/world.geojson'),
+    d3.csv('data/child_mortality_trends.csv')
+])
     .then(data => {
+        const geoData = data[0];
+        const countryData = data[1];
+
         // interpret all points besides entity from csv as numbers instead of strings
-        data.forEach(d => {
+        countryData.forEach(d => {
             d.year = +d.year;
             d.child_mortality_rate = +d.child_mortality_rate;
             d.sanitation = +d['share of the population using at least basic sanitation']
         });
 
         // initialize and render histograms
-        updateHistogram(data, 'child_mortality_rate', 2023, child_mortality_histogram, '#child_mortality_histogram', 'Under-Five Child Mortality', 'Child Mortality Rate (%)');
-        updateHistogram(data, 'sanitation', 2023, sanitation_histogram, '#sanitation_histogram', 'Basic Sanitation Usage', 'Percent of Population Using at Least Basic Sanitation (%)');
+        updateHistogram(countryData, 'child_mortality_rate', 2023, childMortalityHistogram, '#child_mortality_histogram', 'Under-Five Child Mortality', 'Child Mortality Rate (%)');
+        updateHistogram(countryData, 'sanitation', 2023, sanitationHistogram, '#sanitation_histogram', 'Basic Sanitation Usage', 'Percent of Population Using at Least Basic Sanitation (%)');
 
         // initialize and render scatterplot
-        updateScatterplot(data, 'sanitation', 'child_mortality_rate', 2023, scatterplot, '#scatterplot')
+        updateScatterplot(countryData, 'sanitation', 'child_mortality_rate', 2023, scatterplot, '#scatterplot');
+
+        // initialize and render choropleth map
+        updateChoroplethMap(countryData, geoData, 'sanitation', 2023, choroplethMap, '#sanitation_map', 'Percent of Population Using at Least Basic Sanitation (%)');
     })
     .catch(error => console.error(error));
 
